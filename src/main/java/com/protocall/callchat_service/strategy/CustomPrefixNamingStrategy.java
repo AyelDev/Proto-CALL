a53cda12860fc3f7b840f04fb5d2ecd5ad163dfb
Ariel Abelgas <ariabelgas@proweaver.net>
2026-05-27 19:33:07 +0800

    added strategy prefix naming

---
+++ src/main/java/com/protocall/callchat_service/strategy/CustomPrefixNamingStrategy.java
@@ -0,0 +1,42 @@
+package com.protocall.callchat_service.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class CustomPrefixNamingStrategy implements PhysicalNamingStrategy {
+
+    private static final String PREFIX = "prt_";
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment context) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment context) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
+
+        if (name == null) {
+            return null;
+        }
+
+        String tableName = PREFIX + name.getText();
+
+        return Identifier.toIdentifier(tableName);
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment context) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment context) {
+        return name;
+    }
+}
